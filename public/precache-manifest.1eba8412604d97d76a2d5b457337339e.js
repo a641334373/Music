f2b19b608825e8e82218eb5ec18939383efc3615
https://github.com/a641334373/TanHao.git <42881378+a641334373@users.noreply.github.com>
2018-12-11 00:11:06 +0800

    modify

---
+++ public/precache-manifest.1eba8412604d97d76a2d5b457337339e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ebfe59a72f406b67191",
+    "url": "/static/js/main.7ebfe59a.chunk.js"
+  },
+  {
+    "revision": "396a0c1a20bafe718c2e",
+    "url": "/static/js/1.396a0c1a.chunk.js"
+  },
+  {
+    "revision": "7ebfe59a72f406b67191",
+    "url": "/static/css/main.725816a0.chunk.css"
+  },
+  {
+    "revision": "fac8d56d575aa12d1ea8bf951145d55d",
+    "url": "/index.html"
+  }
+];
